b91144c34355df31a26da15cdb2e8d613d816f20
fraankrr <rr.frank@web.de>
2022-01-31 11:21:50 +0100

    loading spinner and form validation

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ button.addEventListener("click", function () {
     modal.classList.toggle("d-none")
 })
 
+const form = document.getElementById("form")
+form.addEventListener("submit", function (event) {
+    event.preventDefault()
+    //TODO SHOW  spinner
+    //TODO collect form entries
+    //TODO validate form entries
+    const loading = document.getElementById("loading")
+    loading.classList.toggle("d-none")
+    const forminputs = event.target.querySelectorAll("[type='text']")
+    const user = {
+        firstname: forminputs[0].value,
+        lastname: forminputs[1].value,
+    }
+    if (user.firstname.length < 2) {
+        alert("firstname too short")
+    }
+    if (user.lastname.length < 2) {
+        alert("lastname too short")
+    }
+    if (user.lastname.indexOf("XXX") !== -1) {
+        alert("Usernme not allowed to use XXXX")
+    }
+})
