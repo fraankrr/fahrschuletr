4a83f4089a5580593a8a722e1fe081ae363e55ab
fraankrr <rr.frank@web.de>
2022-01-31 10:42:17 +0100

    toggle modal form

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ brand.addEventListener("click", function () {
     const nav = document.getElementById("nav")
     nav.classList.toggle("d-none")
 })
+
+const button = document.getElementById("popup-stricter")
+button.addEventListener("click", function () {
+    const modal = document.getElementById("modal")
+    modal.classList.toggle("d-none")
+})
+
